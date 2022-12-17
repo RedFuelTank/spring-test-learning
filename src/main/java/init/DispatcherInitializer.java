e176bb714b21e87f2e8194595ab6d5f22793ac6e
Danila Romanov <danila.romanof@gmail.com>
2022-12-17 10:54:41 +0200

    Initial state

---
+++ src/main/java/init/DispatcherInitializer.java
@@ -0,0 +1,23 @@
+package init;
+
+import config.MainConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{
+                MainConfig.class
+        };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/*"};
+    }
+}
